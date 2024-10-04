e80f930819aab115616a8a9dab0d97715676b754
c0raw <c0rawoff@gmail.com>
2024-10-04 15:16:17 +0200

    update de signup.js, et test.html
    je suis juste le boss mtn ça fonctionne

--- signup.js
+++ signup.js
@@ -1,9 +1,18 @@
-window.onload = () => {
-    document.getElementById('inscription').onsubmit = (e) => {
-        e.preventDefault();
-
-        ///
-
-        e.submit();
+function updateButtons() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    const psw = urlParams.get('psw');
+  
+    if (id && psw) {
+      const navButtons = document.querySelectorAll('nav ul li a');
+      navButtons.forEach((button) => {
+        if (button.textContent === 'Connexion') {
+          button.textContent = `Bonjour, ${id}`;
+        } else if (button.textContent === 'S\'inscrire') {
+          button.style.display = 'none';
+        }
+      });
     }
-}
+  }
+  
+window.onload = updateButtons;
